86e5529fe6b98c9e422bf3e14f528a7f3f1d2d1a
Jeff Cousins <jeffcousins@me.com>
2015-10-08 21:10:18 -0500

    completed data structures

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
+  this.count = 0;
+  
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
-
-
+Stack.prototype = {
+  push: function(value) {
+    this.count++;
+    this[this.count] = value;
+  },
+  pop: function() {
+    var temp = this[this.count];
+    delete this[this.count];
+    
+    this.count--;
+    if ( this.count < 0 ) {
+      this.count = 0;
+      return 0;
+    }
+ 
+    return temp;
+  },
+  size: function() {
+    return this.count;
+  },
+  constructor: Stack
+}
