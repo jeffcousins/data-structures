dadfb06f6ca38a40c7c307a772690828a944dc84
Jeff Cousins <jeffcousins@me.com>
2015-10-09 22:09:31 -0500

    Completed first five

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,42 +3,108 @@
 // ###Graph Solution
 
 // Instantiate a new graph
-var Graph = function(){
-};
+var Graph = function (){
+  this.nodes = [];
+  };
 
-// ------------------------
+var RickRoll = function(value) {
+  this.value = value,
+  this.connections = [];
+}
+
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node){
+Graph.prototype.addNode = function(value) { 
+  this.nodes.push(new RickRoll(value));
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node){
+Graph.prototype.contains = function(value){ 
+  var doesContain = false;
+  _.each(this.nodes, function(item) {
+    if ( item.value === value ) { 
+      doesContain = true;
+    }
+  });
+  return doesContain;
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node){
+  var removed;
+  var index;
+  _.each(this.nodes, function(item, i) {
+    if ( item.value === node ) {
+      removed = node; 
+      index = i;
+    }
+  })
+  this.nodes.splice(index, 1)[0];
+
+  return removed;
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  var result = false;
+  _.each(this.nodes, function(item) {
+    if ( item.value === fromNode ) {
+      _.each(item.connections, function(node){
+        if (node.value === toNode) {
+          result = true;
+        }
+      })
+    }
+  })
+  return result;
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode){
+  var fromIndex;
+  var toIndex;
+   _.each(this.nodes, function(item, i) {
+    if ( item.value === fromNode ) { 
+      fromIndex = i;  
+    } 
+    if ( item.value === toNode ) {
+      toIndex = i;
+    }
+  })
+   this.nodes[fromIndex].connections.push(this.nodes[toIndex]);
+   this.nodes[toIndex].connections.push(this.nodes[fromIndex]);
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  _.each(this.nodes, function(item) {
+    if (item.value === fromNode) {
+      _.each(item.connections, function(node, index) {
+        if (node.value === toNode) {
+          item.connections.splice(index, 1);
+        }
+      });
+
+    } else if (item.value === toNode) {
+      _.each(item.connections, function(node, index) {
+        if (node.value === fromNode) {
+          item.connections.splice(index, 1);
+        }
+      });
+    }
+  });
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb){
+  _.each(this.nodes, function(item) {
+      cb(item.value);
+  });
 };
 
 /*
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,54 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    // first pass check if head is null 
+    if ( list.head === null ) {
+       //if yes add value to head
+      list.tail = Node(value);
+      list.head = list.tail;
+      //list.head.next = list.tail; // added
+    } else if ( list.tail === list.head ) {
+      // if yes alter head and add tail
+      var newNode = Node( value )
+      list.tail = newNode;
+      list.head.next = newNode;
+    } else {
+       // third pass alter tail only
+      var newNode = Node( value );
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+    // take the current list.tail and change it's .next to point to new value
+    // update list.tail as new value
   };
 
   list.removeHead = function(){
+    // find current list.head's .next value and assign it the position of list.head
+    // return the value of the former list.head
+    // delete
+    var currentVal = list.head.value;
+    var futureHead = list.head.next;
+    delete list.head;
+    list.head = futureHead;
+    return currentVal;
   };
 
   list.contains = function(target){
+    // start at head check node.value if true return
+    // if not go to node.next and repeat
+
+    var search = function(node) {
+   
+      if ( node.value === target ) {
+        return true; 
+      } else {
+        if ( node.next ) {
+          return search( node.next )
+        }
+      }
+    };
+
+    return search(list.head) || false;
   };
 
   return list;
@@ -27,3 +69,4 @@ var Node = function(value){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ 
